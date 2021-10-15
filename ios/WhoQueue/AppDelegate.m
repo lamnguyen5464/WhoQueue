08150095b66dacca30a033578bbf82e0a9bffc35
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-10-15 17:47:36 +0700

    integrate: add fb sdk for ios

--- ios/WhoQueue/AppDelegate.m
+++ ios/WhoQueue/AppDelegate.m
@@ -4,49 +4,60 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import "FBSDKCoreKit/FBSDKCoreKit.h"
 
-static void InitializeFlipper(UIApplication *application) {
-  FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-  [client addPlugin:[FlipperKitReactPlugin new]];
-  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-  [client start];
-}
-#endif
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#ifdef FB_SONARKIT_ENABLED
-  InitializeFlipper(application);
-#endif
-
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"WhoQueue"
                                             initialProperties:nil];
-
+  
   if (@available(iOS 13.0, *)) {
-      rootView.backgroundColor = [UIColor systemBackgroundColor];
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
   } else {
-      rootView.backgroundColor = [UIColor whiteColor];
+    rootView.backgroundColor = [UIColor whiteColor];
   }
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  //Config Facebook
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                           didFinishLaunchingWithOptions:launchOptions];
+  
+  NSArray *permissions = @[@"public_profile", @"email"];
+  
+  FBSDKLoginManager *loginMgr = [[FBSDKLoginManager alloc] init];
+  [loginMgr logInWithPermissions:permissions fromViewController:nil handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+    if (error) {
+      NSLog(@"@@@ error");
+      
+    }
+    
+    if (result.isCancelled || !result.token) {
+      NSLog(@"@@@ cancel");
+    }
+    
+    NSInteger expiration = [result.token.expirationDate timeIntervalSince1970];
+    NSLog(@"@@@ sucess: %@", result.token.tokenString);
+//              resolve(@{
+//                        @"type": @"success",
+//                        @"token": result.token.tokenString,
+//                        @"expires": @(expiration),
+//                        @"permissions": [result.token.permissions allObjects],
+//                        @"declinedPermissions": [result.token.declinedPermissions allObjects]
+//                        });
+  }];
+  
   return YES;
 }
 
