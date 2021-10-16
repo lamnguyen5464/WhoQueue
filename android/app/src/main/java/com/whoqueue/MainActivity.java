281d45663f8f36a8ec4177c3952d24534d96051a
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-10-16 14:01:37 +0700

    core: integrate fb sdk for android

--- android/app/src/main/java/com/whoqueue/MainActivity.java
+++ android/app/src/main/java/com/whoqueue/MainActivity.java
@@ -1,15 +1,67 @@
 package com.whoqueue;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.facebook.react.ReactActivity;
+
+import java.util.Arrays;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "WhoQueue";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "WhoQueue";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d("@@@ ", "haehaesfwer");
+
+
+        LoginManager.getInstance().registerCallback(mCallbackManager, null);
+
+        LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile", "email"));
+
+        LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("@@@ token", loginResult.getAccessToken().getToken());
+                Log.d("@@@ exp", loginResult.getAccessToken().getExpires().toString());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("@@@", "Cancel");
+            }
+
+            @Override
+            public void onError(@NonNull FacebookException e) {
+                Log.d("@@@", e.toString());
+            }
+        });
+    }
+
+    CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
